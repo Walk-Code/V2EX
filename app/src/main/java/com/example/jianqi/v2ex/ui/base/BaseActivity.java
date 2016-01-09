05d2a1aba42f26f2d892ec9b22ddd78ddeda9783
walkcode <walk_code@163.com>
2016-01-09 12:59:54 +0800

    commit

---
+++ app/src/main/java/com/example/jianqi/v2ex/ui/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.example.jianqi.v2ex.ui.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jianqi on 2016/1/6.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
